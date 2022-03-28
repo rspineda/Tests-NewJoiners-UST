e0f1ace35b6b86525104f518f09242fee24e8e44
Pilar Pac <pilar.pac@adeccogroup.com>
2022-03-28 17:36:52 +0200

    Exercise 1 solved

--- app2/src/index.ts
+++ app2/src/index.ts
@@ -6,10 +6,19 @@ const newMembers = ['Edu', 'Pil']
 
 //Unelos en un solo array e imprímelo por consola: 
 
+const members1 =  [...members,...newMembers];
+console.log(members1)
+
 
 //2. Crea una función que demuestre el uso de un let partiendo de la varible dada.
 const number = 5;
 
+const myFunction =() => {
+    let number = 3;
+    console.log(number)
+}
+myFunction();
+console.log(number)
 
 //3. / 4. / 5. Dado el objeto newMember --> Crea una función que me imprima por consola el nombre del nuevo miembro
 // con un saludo ameno como: 'Hola Pil que tal  estas' (utilizando arrow function, destructuring y template strings)
@@ -20,9 +29,19 @@ const newMember = {
   likesSport: true
 }
 
+const  saludoMiembro =(newMember) =>{
+    const {name} = newMember
+    //console.log(name)
+    console.log(`Hola ${name} que tal estas`)
+
+}
+saludoMiembro(newMember);
+
 //6. Dado el array celsius, crea un nuevo array (farenheit) que contenga los los elementos del array celsius pero en formato fahrenheit
 //Para pasar de celsius a farenheit: F = (C * 9/5) + 32 
 const celsius = [20, 23, 12, 30, 22]
+const farenheit = celsius.map(elem => (elem*(9/5)) +32)
+console.log(farenheit)
 
 
 //7. apartir de la api dada renderiza por consola los objetos (todos)
